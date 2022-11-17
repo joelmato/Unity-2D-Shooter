473ffeacf6bd61df766f620fd8739742ea2eb1d4
joelmato <joel.tommerbakk@hotmail.com>
2022-11-17 15:06:16 +0100

    Changed the way enemies are spawned

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -15,39 +15,84 @@ public class Spawner : MonoBehaviour
 
     private bool canSpawn = true;
     private float spawnRate = 30;
+    private float timeBetweenWaves = 3;
 
     public int numberOfHearts = 0;
 
+    private GameObject[,] enemyWaves; // Used to store the different combinations of enemies that can be spawned together as a "wave"
+
+    private int minutesCount = 0;
+    private float secondsCount = 0;
+
+    void Start()
+    {
+        // Fill enemyWaves array with different enemy combinations
+        enemyWaves = new GameObject[,] {
+            { zombieNormalPrefab, zombieNormalPrefab, zombieNormalPrefab, zombieNormalPrefab },
+            { zombieWithPistolPrefab, zombieWithPistolPrefab, zombieWithPistolPrefab, zombieWithPistolPrefab },
+            { zombieWithShotgunPrefab, zombieWithShotgunPrefab, zombieWithShotgunPrefab, zombieWithShotgunPrefab },
+            { zombieNormalPrefab , zombieNormalPrefab, zombieWithPistolPrefab, zombieWithPistolPrefab },
+            { zombieNormalPrefab , zombieNormalPrefab, zombieWithShotgunPrefab, zombieWithShotgunPrefab },
+            { zombieWithPistolPrefab , zombieWithPistolPrefab, zombieWithShotgunPrefab, zombieWithShotgunPrefab },
+            { zombieNormalPrefab , zombieNormalPrefab, zombieWithPistolPrefab, zombieWithShotgunPrefab },
+            { zombieWithPistolPrefab, zombieWithPistolPrefab, zombieWithBombPrefab, zombieWithBombPrefab },
+            { zombieWithShotgunPrefab, zombieWithShotgunPrefab, zombieWithBombPrefab, zombieWithBombPrefab },
+            { zombieNormalPrefab, zombieWithPistolPrefab, zombieWithShotgunPrefab, zombieWithBombPrefab },
+        };
+    }
     void Update()
     {
+        GetTime();
+
         if (canSpawn)
         {
-            SpawnZombies();
+            StartCoroutine(StartCooldown());
         }
     }
 
-    // Method that spawns/instantiates zombies at the spawn points
-    void SpawnZombies()
+    // Method that spawns/instantiates random zombies at the spawn points,
+    // either 4 if spawnTwoAtRandom is False or 2 if spawnTwoAtRandom is True
+    void SpawnZombies(bool spawnTwoAtRandom)
     {
-        for (int i = 0; i < 1; i++)
-        {
-            GameObject zombie = Instantiate(zombieNormalPrefab);
-            GameObject zombieWP = Instantiate(zombieWithPistolPrefab);
-            zombie.transform.position = spawnpoints[0].transform.position;
-            zombieWP.transform.position = spawnpoints[1].transform.position;
+        int random = Random.Range(0, enemyWaves.GetLength(0) - 1);
 
-            GameObject zombie2 = Instantiate(zombieWithBombPrefab);
-            GameObject zombieWP2 = Instantiate(zombieWithShotgunPrefab);
-            zombie2.transform.position = spawnpoints[2].transform.position;
-            zombieWP2.transform.position = spawnpoints[3].transform.position;
+        if (spawnTwoAtRandom)
+        {
+            int random2 = Random.Range(0, 1);
+            GameObject zombie = Instantiate(enemyWaves[random, random2]);
+            zombie.transform.position = spawnpoints[random2].transform.position;
+
+            random2 = Random.Range(2, 3);
+            GameObject zombie2 = Instantiate(enemyWaves[random, random2]);
+            zombie2.transform.position = spawnpoints[random2].transform.position;
         }
-        StartCoroutine(StartCooldown());
+        else
+        {
+            for (int j = 0; j < enemyWaves.GetLength(1); j++)
+            {
+                GameObject zombie = Instantiate(enemyWaves[random, j]);
+                zombie.transform.position = spawnpoints[j].transform.position;
+            }
+        }
     }
 
     IEnumerator StartCooldown()
     {
         canSpawn = false;
-        yield return new WaitForSeconds(spawnRate);
+
+        SpawnZombies(false); // Spawns 4 random zombies
+        yield return new WaitForSeconds(timeBetweenWaves);
+
+        // 2 * minutesCount number of zombies are spawned
+        for (int i = 0; i < minutesCount; i++)
+        {
+            SpawnZombies(true); // Spawns 2 random zombies
+
+            yield return new WaitForSeconds(timeBetweenWaves);
+        }
+
+        // Waits until the next batch of zombies can spawn
+        yield return new WaitForSeconds(spawnRate - timeBetweenWaves - minutesCount * timeBetweenWaves);
         canSpawn = true;
     }
 
@@ -63,6 +108,18 @@ public class Spawner : MonoBehaviour
                 GameObject heartPowerUp = Instantiate(heartPowerUpPrefab, position, Quaternion.identity);
                 numberOfHearts++;
             }
+        }
+    }
+
+    // Gets the current game time in minutes and seconds
+    public void GetTime()
+    {
+        secondsCount += Time.deltaTime;
+
+        if (secondsCount >= 60)
+        {
+            secondsCount %= 60;
+            minutesCount++;
         }
     }
 }
